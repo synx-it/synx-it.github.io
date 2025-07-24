5ed80119a826c6b785048fa3c375cf07dd257209
gfacchi-dev <giuseppe.facchi@outlook.com>
2025-07-24 11:46:42 -0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/synx-web',
-    trailingSlash: true,
+    basePath: '/website',
     images: {
         unoptimized: true,
     },
