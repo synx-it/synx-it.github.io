c14a8a18693a272702356f655da5c5bc0ab69ec2
gfacchi-dev <giuseppe.facchi@outlook.com>
2025-07-25 11:39:11 -0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  // Remove basePath and assetPrefix for GitHub Pages root URL
-  // or set them to empty strings if you want to deploy to the root of your domain
-  basePath: '/website',
-  assetPrefix: '/website',
-  trailingSlash: true,
 };
 
 export default nextConfig;
