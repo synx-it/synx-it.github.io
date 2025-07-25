a1cf84b71e68b8ac8cd7ddcc7b292d3ccadbf9bd
gfacchi-dev <giuseppe.facchi@outlook.com>
2025-07-25 11:43:11 -0700

    Prova

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: 'export',
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  basePath: "/website",
 };
 
 export default nextConfig;
